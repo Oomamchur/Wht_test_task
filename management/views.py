2249065dc484b40de1cd02b69ccbb7715b8ab84d
Oleksandr Mamchur <irex1988@gmail.com>
2023-10-18 22:27:59 +0300

    added views

--- management/views.py
+++ management/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from management.models import Team, Human
+from management.serializers import (
+    TeamSerializer,
+    TeamListSerializer,
+    HumanSerializer,
+    HumanListSerializer,
+)
+
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return TeamListSerializer
+        return TeamSerializer
+
+
+class HumanViewSet(viewsets.ModelViewSet):
+    queryset = Human.objects.all()
+    serializer_class = HumanSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return HumanListSerializer
+        return HumanSerializer
