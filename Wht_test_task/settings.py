197b0776035672fc548016f229afc73f598e77b9
Oleksandr Mamchur <irex1988@gmail.com>
2023-10-21 00:00:00 +0300

    made email login instead of username

--- Wht_test_task/settings.py
+++ Wht_test_task/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "debug_toolbar",
     "drf_spectacular",
     "management",
+    "user"
 ]
 
 MIDDLEWARE = [
@@ -112,6 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
